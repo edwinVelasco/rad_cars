ed888b07b03ea6ca03d8d4e18eac1fb37663a136
edwin <edwinalbertovelasco8@gmail.com>
2023-08-13 10:43:57 -0500

    _get_object, private functions

--- api/views.py
+++ api/views.py
@@ -34,19 +34,19 @@ class ProviderView(APIView):
 
 
 class DetailProviderView(APIView):
-    def get_object(self, pk):
+    def _get_object(self, pk):
         try:
             return Provider.objects.get(id=pk)
         except Provider.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
-        provider = self.get_object(pk)
+        provider = self._get_object(pk)
         provider_json = ProviderSerializer(provider)
         return Response(provider_json.data, status=200)
 
     def put(self, request, pk):
-        provider = self.get_object(pk)
+        provider = self._get_object(pk)
         provider_json = ProviderSerializer(provider, data=request.data)
         if provider_json.is_valid():
             provider_json.save()
@@ -55,7 +55,7 @@ class DetailProviderView(APIView):
         return Response(provider_json.errors, status=400)
 
     def delete(self, request, pk):
-        provider = self.get_object(pk)
+        provider = self._get_object(pk)
         provider.delete()
         return Response(status=204)
 
@@ -126,22 +126,22 @@ class ProductView(APIView):
 
 
 class DetailProductView(APIView):
-    def get_object(self, pk):
+    def _get_object(self, pk):
         try:
             return Product.objects.get(id=pk)
         except Product.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
-        product = self.get_object(pk)
+        product = self._get_object(pk)
         product_json = ProductSerializer(product)
         return Response(product_json.data, status=200)
 
     def put(self, request, pk):
-        product = self.get_object(pk)
+        product = self._get_object(pk)
         product_data = ProductSchemas(**request.data).dict()
         product_json = ProductSerializerCreate(product, data=product_data)  # UnMarshall
-        # product = self.get_object(pk)
+        # product = self._get_object(pk)
         # product_json = ProductSerializer(product, data=request.data)
         if product_json.is_valid():
             product_json.save()
@@ -151,7 +151,7 @@ class DetailProductView(APIView):
 
     def delete(self, request, pk):
         try:
-            product = self.get_object(pk)
+            product = self._get_object(pk)
             product.delete()
             return Response(status=204)
         except ProtectedError:
@@ -177,19 +177,19 @@ class QuotationView(APIView):
 
 
 class DetailQuotationView(APIView):
-    def get_object(self, pk):
+    def _get_object(self, pk):
         try:
             return Quotation.objects.get(id=pk)
         except Quotation.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
-        quotation = self.get_object(pk)
+        quotation = self._get_object(pk)
         quotation_json = QuotationSerializer(quotation)
         return Response(quotation_json.data, status=200)
 
     def put(self, request, pk):
-        quotation = self.get_object(pk)
+        quotation = self._get_object(pk)
         quotation_json = QuotationSerializer(quotation, data=request.data)
         if quotation_json.is_valid():
             quotation_json.save()
@@ -198,7 +198,7 @@ class DetailQuotationView(APIView):
         return Response(quotation_json.errors, status=400)
 
     def delete(self, request, pk, pk2):
-        quotation = self.get_object(pk2)
+        quotation = self._get_object(pk2)
         if quotation.product_id == pk:
             quotation.delete()
             return Response(status=204)
@@ -207,13 +207,13 @@ class DetailQuotationView(APIView):
 
 class MarkView(APIView):
 
-    def get_models(self, mark_id):
+    def _get_models(self, mark_id):
         models = Model.objects.filter(mark_id=mark_id)
         return [{'id': model.id, 'name': model.name} for model in models]
 
     def get(self, request):
         marks = Mark.objects.all()
-        marks_response = [{"id": mark.id, "name": mark.name, "models": self.get_models(mark_id=mark.id)} for mark in marks]
+        marks_response = [{"id": mark.id, "name": mark.name, "models": self._get_models(mark_id=mark.id)} for mark in marks]
         return Response(marks_response)
 
     def post(self, request):
@@ -225,19 +225,19 @@ class MarkView(APIView):
 
 
 class DetailMarkView(APIView):
-    def get_object(self, pk):
+    def _get_object(self, pk):
         try:
             return Mark.objects.get(id=pk)
         except Mark.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
-        product = self.get_object(pk)
+        product = self._get_object(pk)
         product_json = MarkSerializer(product)
         return Response(product_json.data, status=200)
 
     def put(self, request, pk):
-        mark = self.get_object(pk)
+        mark = self._get_object(pk)
         mark_json = MarkSerializer(mark, data=request.data)
         if mark_json.is_valid():
             mark_json.save()
@@ -247,7 +247,7 @@ class DetailMarkView(APIView):
 
     def delete(self, request, pk):
         try:
-            mark = self.get_object(pk)
+            mark = self._get_object(pk)
             mark.delete()
             return Response(status=204)
         except ProtectedError:
@@ -270,19 +270,19 @@ class ModelView(APIView):
 
 
 class DetailModelView(APIView):
-    def get_object(self, pk):
+    def _get_object(self, pk):
         try:
             return Model.objects.get(id=pk)
         except Model.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
-        model = self.get_object(pk)
+        model = self._get_object(pk)
         model_json = ModelSerializer(model)
         return Response(model_json.data, status=200)
 
     def put(self, request, pk):
-        model = self.get_object(pk)
+        model = self._get_object(pk)
         model_json = ModelSerializerCreate(model, data=request.data)
         if model_json.is_valid():
             model_json.save()
@@ -292,7 +292,7 @@ class DetailModelView(APIView):
 
     def delete(self, request, pk):
         try:
-            model = self.get_object(pk)
+            model = self._get_object(pk)
             model.delete()
             return Response(status=204)
         except ProtectedError:
@@ -315,19 +315,19 @@ class CategoryView(APIView):
 
 
 class DetailCategoryView(APIView):
-    def get_object(self, pk):
+    def _get_object(self, pk):
         try:
             return Category.objects.get(id=pk)
         except Model.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
-        category = self.get_object(pk)
+        category = self._get_object(pk)
         category_json = CategorySerializer(category)
         return Response(category_json.data, status=200)
 
     def put(self, request, pk):
-        category = self.get_object(pk)
+        category = self._get_object(pk)
         category_json = CategorySerializer(category, data=request.data)
         if category_json.is_valid():
             category_json.save()
@@ -337,7 +337,7 @@ class DetailCategoryView(APIView):
 
     def delete(self, request, pk):
         try:
-            category = self.get_object(pk)
+            category = self._get_object(pk)
             category.delete()
             return Response(status=204)
         except ProtectedError:
